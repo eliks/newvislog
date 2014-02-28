68b16eb1e9dd4085b38d860ae8026e4e0ebd4ee2
Elisha Senoo <elisha@rightcomtech.com>
2014-02-28 11:22:54 +0000

    wow

--- app/config/app.php
+++ app/config/app.php
@@ -16,12 +16,12 @@ return array(
 	'debug' => false,
 
 	/*
-	|--------------------------------------------------------------------------
+	|-888-------------------------------------------------------------------------
 	| Application URL
 	|--------------------------------------------------------------------------
 	|
 	| This URL is used by the console to properly generate URLs when using
-	| the Artisan command line tool. You should set this to the root of
+	| the Artisan command  line tool. You should set this to the root of
 	| your application so that it is used when running Artisan tasks.
 	|
 	*/
